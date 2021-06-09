4f6a22f6bd0b702c0fb9fab793a3a76836ac8b5a
Mohammad Shohag <shohagcsediu@gmail.com>
2021-06-09 21:49:01 +0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="flex items-center justify-between w-10/12 mx-auto py-3">
+                <h1>Programming Hero</h1>
+                <p>Login</p>
+                             
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
